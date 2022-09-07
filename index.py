19c0ebdd948a1297300fb9fc3d1d01c8104fd737
Zentriex <zentriexgg@gmail.com>
2022-09-07 12:07:34 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,30 @@
+import discord
+import dotenv
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from discord.ext import commands
+
+client = discord.client()
+bot = commands.Bot(command_prefix="e!")
+
+@client.event
+async def on_ready():
+    print("Logged on as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    message.content = message.content.lower().replace(" ", "")
+
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith("e!help"):
+        await message.channel.send("This is a help menu without any embeds because I have not added any as of yet.")
+
+    if message.content.startswith("e!twitter"):
+        await message.channel.send("https://www.twitter.com/excane")
+
+client.run(os.getenv("DISCORD_TOKEN"))
